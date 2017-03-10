1f71dd725434dedbaa8956cdf1b33bcb9b280f32
moggot <9.tilov@gmail.com>
2017-03-10 13:34:41 +0300

    подправил стили

---
+++ app/src/main/java/com/moggot/commonalarmclock/ActivityAlarm.java
@@ -0,0 +1,15 @@
+package com.moggot.commonalarmclock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.moggot.alarmclock.R;
+
+public class ActivityAlarm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+    }
+}
