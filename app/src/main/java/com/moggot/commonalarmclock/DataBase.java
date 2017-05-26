dc6c9bb5a2dcaccc3c39de2f7e5b02dcb6ef7095
moggot <9.tilov@gmail.com>
2017-05-26 11:31:29 +0300

    добавлена сортировка будильников по времени

--- app/src/main/java/com/moggot/commonalarmclock/DataBase.java
+++ app/src/main/java/com/moggot/commonalarmclock/DataBase.java
@@ -1,7 +1,7 @@
 package com.moggot.commonalarmclock;
 
 import android.content.Context;
-import android.support.v7.util.SortedList;
+import android.util.Log;
 import android.util.SparseIntArray;
 
 import com.google.gson.Gson;
@@ -11,11 +11,18 @@ import com.moggot.commonalarmclock.alarm.AlarmDao;
 import com.moggot.commonalarmclock.alarm.DaoSession;
 
 import java.lang.reflect.Type;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Random;
 
 public class DataBase {
+
+    private static final String LOG_TAG = DataBase.class.getSimpleName();
 
     private Context applicationContext;
     private AlarmDao alarmDao;
@@ -42,7 +49,21 @@ public class DataBase {
     }
 
     public List<Alarm> getAllAlarms() {
-        return alarmDao.queryBuilder().orderAsc(AlarmDao.Properties.Date).build().list();
+        List<Alarm> alarms = alarmDao.queryBuilder().build().list();
+        sortByTime(alarms);
+        return alarms;
+    }
+
+    private void sortByTime(List<Alarm> alarms) {
+        Collections.sort(alarms, new Comparator<Alarm>() {
+            @Override
+            public int compare(Alarm o1, Alarm o2) {
+                DateFormat localDateFormat = new SimpleDateFormat("HH:mm:ss", Locale.US);
+                String time1 = localDateFormat.format(o1.getDate());
+                String time2 = localDateFormat.format(o2.getDate());
+                return time1.compareTo(time2);
+            }
+        });
     }
 
     private AlarmDao setupDb() {
