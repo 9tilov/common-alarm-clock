278fb46fada58172218fc34ddaa9e009cc53e7ff
moggot <9.tilov@gmail.com>
2017-06-01 09:56:38 +0300

    try3

--- app/src/main/java/com/moggot/commonalarmclock/SwipeRecyclerViewAdapter.java
+++ app/src/main/java/com/moggot/commonalarmclock/SwipeRecyclerViewAdapter.java
@@ -14,6 +14,7 @@ public class SwipeRecyclerViewAdapter extends RecyclerView.Adapter<AlarmViewHold
     public SwipeRecyclerViewAdapter(MainPresenter presenter) {
         this.presenter = presenter;
 
+
         setHasStableIds(true);
     }
 
